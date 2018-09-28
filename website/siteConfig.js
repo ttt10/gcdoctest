665e63e2cab68db8c4b9736c1440d37df588a7bf
dinoreborn <43055340+dinoreborn@users.noreply.github.com>
2018-09-28 16:01:24 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
   editUrl: 'https://github.com/dinoreborn/gcdoctest/tree/master/docs/',
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'Overview', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
